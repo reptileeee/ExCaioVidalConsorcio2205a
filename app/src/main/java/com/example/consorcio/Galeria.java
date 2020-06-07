c09c4516226bea40d53a595458df0e19f1bc1a83
reptileeee <65796757+reptileeee@users.noreply.github.com>
2020-06-06 23:01:56 -0300

    Exercicio 06/06 Aline

---
+++ app/src/main/java/com/example/consorcio/Galeria.java
@@ -0,0 +1,94 @@
+package com.example.consorcio;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Galeria extends AppCompatActivity {
+    private static final int CAPTURAR_IMAGEM = 1;
+    private Uri uri;
+
+
+    @SuppressLint("MissingPermission")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+    public void capturarImagem(View v) {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(intent, CAPTURAR_IMAGEM);
+        }
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAPTURAR_IMAGEM) {
+            if (resultCode == RESULT_OK) {
+                Bundle extras = data.getExtras();
+                Bitmap imageBitmap = (Bitmap) extras.get("data");
+                ImageView imageView = (ImageView) findViewById(R.id.imageView);
+                imageView.setImageBitmap(imageBitmap);
+                mostrarMensagem("Imagem capturada!");
+                adicionarNaGaleria();
+            } else {
+                mostrarMensagem("Imagem não capturada!");
+            }
+        }
+        if (resultCode == Activity.RESULT_OK && requestCode == 123)
+        {
+            Uri imagemSelecionada = data.getData();
+            //ImageView imageView = (ImageView) findViewById(R.id.imageView);
+            ImageView imageView = new ImageView(this);
+            imageView.setImageURI(imagemSelecionada);
+            LinearLayout ln = (LinearLayout)findViewById(R.id.layoutLinear);
+            ln.addView(imageView);
+        }
+    }
+
+
+    private void mostrarMensagem(String msg){
+        Toast.makeText(this, msg,
+                Toast.LENGTH_LONG)
+                .show();
+    }
+    /*imagem não é adicionada automaticamente a galeria
+    é necessário chamar um serviço de notificação (broadcast para que a imagem seja adicionada a galeria
+
+     */
+    private void adicionarNaGaleria() {
+        Intent intent = new Intent(
+                Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        intent.setData(uri);
+        this.sendBroadcast(intent);
+    }
+
+    public void visualizarImagem(View v){
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setDataAndType(uri, "image/jpeg");
+        startActivity(intent);
+    }
+
+    public void visualizarGaleria(View view) {
+        Intent intent =     new Intent(Intent.ACTION_PICK,
+                android.provider.MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        startActivityForResult(Intent.createChooser(intent, "Selecione uma imagem"), 123);
+    }
+
+}
